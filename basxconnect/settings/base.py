60e3b5ef5a428e87ec62f2616b281f8f21785112
sam <sam@basx.dev>
2020-09-07 11:33:41 +0700

    did: update according to refactored API of bread engine

--- basxconnect/settings/base.py
+++ basxconnect/settings/base.py
@@ -33,16 +33,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = ["person"] + BREAD_DEPENDENCIES
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-
 ROOT_URLCONF = "basxconnect.urls"
 
 TEMPLATES = [
--- person/bread.py
+++ person/bread.py
@@ -1,3 +1,4 @@
+from bread import views
 from bread.admin import BreadAdmin, register
 from bread.forms.layout import InlineLayout, Row, Tab, Tabs
 from crispy_forms.layout import Div, Fieldset, Layout
@@ -9,67 +10,73 @@ from . import models
 @register
 class Person(BreadAdmin):
     model = models.Person
-    browsefields = [
-        "name",
-        "preferred_channel",
-    ]
-    editfields = Layout(
-        Tabs(
-            Tab(_("Person"), "name"),
-            Tab(
-                _("Addresses"),
-                Tabs(
-                    Tab(
-                        "Postal Addresses",
-                        InlineLayout(
-                            "person_postal_list",
-                            Row.with_columns(
-                                ("type", 2), ("country", 2), ("address", 8)
+    browse_view = views.BrowseView.customize(fields=["name", "preferred_channel"])
+    edit_view = views.EditView.customize(
+        fields=Layout(
+            Tabs(
+                Tab(_("Person"), "name"),
+                Tab(
+                    _("Addresses"),
+                    Tabs(
+                        Tab(
+                            "Postal Addresses",
+                            InlineLayout(
+                                "person_postal_list",
+                                Row.with_columns(
+                                    ("type", 2), ("country", 2), ("address", 8)
+                                ),
                             ),
                         ),
-                    ),
-                    Tab(
-                        "Email Addresses",
-                        InlineLayout(
-                            "person_email_list",
-                            Row.with_columns(("type", 2), ("email", 10)),
+                        Tab(
+                            "Email Addresses",
+                            InlineLayout(
+                                "person_email_list",
+                                Row.with_columns(("type", 2), ("email", 10)),
+                            ),
                         ),
-                    ),
-                    Tab(
-                        "Phone Numbers",
-                        InlineLayout(
-                            "person_phone_list",
-                            Row.with_columns(("type", 2), ("number", 10)),
+                        Tab(
+                            "Phone Numbers",
+                            InlineLayout(
+                                "person_phone_list",
+                                Row.with_columns(("type", 2), ("number", 10)),
+                            ),
                         ),
                     ),
                 ),
             ),
-        ),
+        )
     )
+    add_view = views.AddView.customize(fields=edit_view.fields)
 
 
 @register
 class AddressType(BreadAdmin):
     model = models.AddressType
-    browsefields = []
+    browse_view = views.BrowseView.customize(fields=[])
 
 
 @register
 class Relationship(BreadAdmin):
     model = models.Relationship
-    editfields = Layout(
-        Div(
+    edit_view = views.EditView.customize(
+        fields=Layout(
             Div(
-                Fieldset(_("Relationship"), "type", "person_a", "person_b"),
-                css_class="col s6",
-            ),
-            Div(Fieldset(_("Duration"), "start_date", "end_date"), css_class="col s6",),
-            css_class="row",
+                Div(
+                    Fieldset(_("Relationship"), "type", "person_a", "person_b"),
+                    css_class="col s6",
+                ),
+                Div(
+                    Fieldset(_("Duration"), "start_date", "end_date"),
+                    css_class="col s6",
+                ),
+                css_class="row",
+            )
         )
     )
+    add_view = views.AddView.customize(fields=edit_view.fields)
 
 
 @register
 class RelationshipType(BreadAdmin):
     model = models.RelationshipType
-    browsefields = []
+    browse_view = views.BrowseView.customize(fields=[])
