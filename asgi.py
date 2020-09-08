91b9a20b7867cdd9b7679c23146c9e8697f80ed3
sam <sam@basx.dev>
2020-09-08 17:20:35 +0700

    fix: black errors

--- asgi.py
+++ asgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "basxconnect.settings.production"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basxconnect.settings.production")
 
 application = get_asgi_application()
--- wsgi.py
+++ wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "basxconnect.settings.production"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basxconnect.settings.production")
 
 application = get_wsgi_application()
