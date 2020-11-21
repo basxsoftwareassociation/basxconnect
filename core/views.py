41f4abe6feab689ae8e521163ca0ed44f35ebc5f
sam <sam@basx.dev>
2020-11-21 15:13:18 +0700

    add: empty views

--- core/views.py
+++ core/views.py
@@ -1,28 +1,39 @@
+from django.shortcuts import render
+
 from bread.utils.urlgenerator import registerurl
-from bread.utils.views import render_layout_to_response
 from htmlgenerator import DIV
 
 
 @registerurl
 def generalsettings(request):
-    return render_layout_to_response(request, DIV("Hello"))
+    return render(
+        request, "bread/dynamic_layout.html", {"view": {"layout": DIV("Hello")}}
+    )
 
 
 @registerurl
 def appearancesettings(request):
-    return render_layout_to_response(request, DIV("Hello"))
+    return render(
+        request, "bread/dynamic_layout.html", {"view": {"layout": DIV("Hello")}}
+    )
 
 
 @registerurl
 def personssettings(request):
-    return render_layout_to_response(request, DIV("Hello"))
+    return render(
+        request, "bread/dynamic_layout.html", {"view": {"layout": DIV("Hello")}}
+    )
 
 
 @registerurl
 def relationshipssettings(request):
-    return render_layout_to_response(request, DIV("Hello"))
+    return render(
+        request, "bread/dynamic_layout.html", {"view": {"layout": DIV("Hello")}}
+    )
 
 
 @registerurl
 def apikeyssettings(request):
-    return render_layout_to_response(request, DIV("Hello"))
+    return render(
+        request, "bread/dynamic_layout.html", {"view": {"layout": DIV("Hello")}}
+    )
