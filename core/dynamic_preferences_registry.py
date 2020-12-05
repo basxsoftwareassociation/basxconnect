0491b1fa273a213ec74f2af7db898738e25b6d62
sam <sam@basx.dev>
2020-12-05 21:09:54 +0700

    add: verbose names for settings

--- core/dynamic_preferences_registry.py
+++ core/dynamic_preferences_registry.py
@@ -1,9 +1,10 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from dynamic_preferences.preferences import Section
 from dynamic_preferences.registries import global_preferences_registry
 from dynamic_preferences.types import FilePreference, LongStringPreference
 
-general = Section("general")
+general = Section("general", _("General"))
 
 
 @global_preferences_registry.register
@@ -11,6 +12,7 @@ class SenderLetterhead(LongStringPreference):
     section = general
     name = "sender_letterhead"
     default = ""
+    verbose_name = _("Sender letterhead")
 
 
 @global_preferences_registry.register
@@ -19,6 +21,7 @@ class Logo(FilePreference):
     name = "logo"
     field_class = forms.ImageField
     default = ""
+    verbose_name = _("Logo")
 
     field_kwargs = {
         "required": False,
