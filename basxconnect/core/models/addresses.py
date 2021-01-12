84f8ffa54a4f056f3cf4a05dcb5e6a380b437529
autoisort <autoisort@basx.org>
2021-01-12 05:41:46 +0000

    fixup: Format Python code with isort

--- basxconnect/core/models/addresses.py
+++ basxconnect/core/models/addresses.py
@@ -1,9 +1,8 @@
-from django_countries.fields import CountryField
-
 from django.db import models
 from django.template.defaultfilters import linebreaksbr
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
 
 from .persons import Person
