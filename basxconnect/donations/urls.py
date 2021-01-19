3213f369f3a22a8e2acc2d514fb8646b60c53b7b
Sam <sam@basx.dev>
2021-01-19 12:34:02 +0700

    did: add wizard view

--- basxconnect/donations/urls.py
+++ basxconnect/donations/urls.py
@@ -1,7 +1,11 @@
-from bread.utils.urls import default_model_paths
+from bread.utils.urls import default_model_paths, generate_path
 
 from . import models
+from .wizards.donationsimport import DonationsImportWizard
 
 urlpatterns = [
     *default_model_paths(models.Donation),
+    generate_path(
+        DonationsImportWizard.as_view(url_name="importdonations"), "importdonations"
+    ),
 ]
