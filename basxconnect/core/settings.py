1a91b079cabe48cdcca148a1fa3894d1686600cb
Sam <sam@basx.dev>
2021-01-19 12:44:45 +0700

    fix: fallback on missing settings

--- basxconnect/core/settings.py
+++ basxconnect/core/settings.py
@@ -3,6 +3,8 @@ from languages.languages import LANGUAGES
 
 DEFAULTS = {"PREFERRED_LANGUAGES": ("en",)}
 
-preferred_codes = getattr(settings, "BASXCONNECT", DEFAULTS)["PREFERRED_LANGUAGES"]
+preferred_codes = getattr(settings, "BASXCONNECT", {}).get(
+    "PREFERRED_LANGUAGES", DEFAULTS["PREFERRED_LANGUAGES"]
+)
 PREFERRED_LANGUAGES = [i for i in LANGUAGES if i[0] in preferred_codes]
 PREFERRED_LANGUAGES.sort(key=lambda e: preferred_codes.index(e[0]))
