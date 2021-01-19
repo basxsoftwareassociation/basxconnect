4048a8854b218f70880cd403a11a0989d067afa7
Sam <sam@basx.dev>
2021-01-19 12:17:03 +0700

    add: labels and translations for donations

--- basxconnect/donations/models.py
+++ basxconnect/donations/models.py
@@ -1,5 +1,6 @@
 from basxconnect.core.models import Person
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from djmoney.models.fields import CurrencyField
 from djmoney.settings import CURRENCY_CHOICES
 
@@ -12,8 +13,16 @@ class DonationImport(models.Model):
 
 class Donation(models.Model):
     _import = models.ForeignKey(DonationImport, null=True, on_delete=models.CASCADE)
-    person = models.ForeignKey(Person, null=True, on_delete=models.SET_NULL)
-    amount = models.DecimalField(max_digits=12, decimal_places=2)
-    currency = CurrencyField(choices=CURRENCY_CHOICES)
+    _import.verbose_name = _("Import")
+    person = models.ForeignKey(
+        Person, null=True, on_delete=models.SET_NULL, related_name="donations"
+    )
+    person.verbose_name = _("Person")
+    amount = models.DecimalField(_("Amount"), max_digits=12, decimal_places=2)
+    currency = CurrencyField(_("Currency"), choices=CURRENCY_CHOICES)
     currency.lazy_choices = lambda *args: settings.PREFERRED_CURRENCIES
-    date = models.DateField()
+    date = models.DateField(_("Date"))
+
+    class Meta:
+        verbose_name = _("Donation")
+        verbose_name_plural = _("Donations")
