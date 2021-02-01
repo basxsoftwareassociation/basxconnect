f8690d6dd20c0e72361874a81d900213acc5a50a
Sam <sam@basx.dev>
2021-02-01 14:27:49 +0700

    add: menu item for contribution import

--- basxconnect/donations/urls.py
+++ basxconnect/donations/urls.py
@@ -1,4 +1,6 @@
-from bread.utils.urls import default_model_paths, generate_path
+from bread import menu
+from bread.utils.urls import default_model_paths, generate_path, reverse
+from django.utils.translation import gettext_lazy as _
 
 from . import models
 from .wizards.donationsimport import DonationsImportWizard
@@ -9,3 +11,15 @@ urlpatterns = [
         DonationsImportWizard.as_view(url_name="importdonations"), "importdonations"
     ),
 ]
+
+importgroup = menu.Group(_("Imports"), icon="document--import")
+
+menu.registeritem(
+    menu.Item(
+        menu.Link(
+            reverse("importdonations", kwargs={"step": "1"}),
+            _("Import contributions"),
+        ),
+        importgroup,
+    )
+)
