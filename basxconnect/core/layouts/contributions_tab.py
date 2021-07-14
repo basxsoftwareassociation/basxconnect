041e5575971e5bb180c665b47bb25dd7016c5a59
Pascal Wiesmann <pascal.wiesmann@outlook.com>
2021-07-14 12:22:30 +0200

    remove unnecessary wrappers

--- basxconnect/core/layouts/contributions_tab.py
+++ basxconnect/core/layouts/contributions_tab.py
@@ -16,31 +16,25 @@ def contributions_tab(request):
     person = get_object_or_404(Person, pk=request.resolver_match.kwargs["pk"])
     return layout.tabs.Tab(
         _("Contributions"),
-        hg.BaseElement(
-            R(
-                C(
-                    layout.datatable.DataTable.from_queryset(
-                        person.contributions.all(),
-                        columns=[
-                            "_import.date",
-                            "date",
-                            "note",
-                            "debitaccount",
-                            "creditaccount",
-                            "amount_formatted",
-                        ],
-                        title="",
-                        addurl=reverse_model(
-                            Contribution,
-                            "add",
-                            query={
-                                "person": person.id,
-                            },
-                        ),
-                        backurl=request.get_full_path(),
-                        prevent_automatic_sortingnames=True,
-                    ),
-                ),
+        layout.datatable.DataTable.from_queryset(
+            person.contributions.all(),
+            columns=[
+                "_import.date",
+                "date",
+                "note",
+                "debitaccount",
+                "creditaccount",
+                "amount_formatted",
+            ],
+            title="",
+            addurl=reverse_model(
+                Contribution,
+                "add",
+                query={
+                    "person": person.id,
+                },
             ),
+            backurl=request.get_full_path(),
+            prevent_automatic_sortingnames=True,
         ),
     )
