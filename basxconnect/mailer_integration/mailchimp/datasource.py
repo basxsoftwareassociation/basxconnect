ac43f3fb0ec85c6165ca38cce3bf397876180e10
Pascal Wiesmann <pascal.wiesmann@outlook.com>
2021-08-19 10:06:13 +0200

    use settings to store mailchimp ids

--- basxconnect/mailer_integration/mailchimp/datasource.py
+++ basxconnect/mailer_integration/mailchimp/datasource.py
@@ -7,20 +7,19 @@ from basxconnect.mailer_integration.abstract import abstract_datasource
 from basxconnect.mailer_integration.abstract.abstract_person_reader import PersonReader
 from basxconnect.mailer_integration.mailchimp import person_reader
 
-URBANMOSAIC_ALL_MEMBERS_LIST_ID = "4606fb0179"
-SWISS_SEGMENT_ID = "36705"
-
 
 class MailchimpDatasource(abstract_datasource.Datasource):
     def __init__(self) -> None:
         self.client = mailchimp_marketing.Client()
-        self.client.set_config({"api_key": settings.MAILCHIMP_API_KEY, "server": "us5"})
+        self.client.set_config(
+            {"api_key": settings.MAILCHIMP_API_KEY, "server": settings.MAILCHIMP_SERVER}
+        )
         self._person_reader = person_reader.MailchimpPersonReader()
 
     def get_persons(self) -> List[Any]:
         swiss_segment = self.client.lists.get_segment_members_list(
-            list_id=URBANMOSAIC_ALL_MEMBERS_LIST_ID,
-            segment_id=SWISS_SEGMENT_ID,
+            list_id=settings.MAILCHIMP_LIST_ID,
+            segment_id=settings.MAILCHIMP_SEGMENT_ID,
             count=1000,
             include_cleaned=True,
             include_transactional=True,
