a9215dc77293d8d4a6192e0947bc65fcca01d5f8
Pascal Wiesmann <pascal.wiesmann@outlook.com>
2021-08-23 15:19:49 +0200

    add migrations

--- basxconnect/mailer_integration/models.py
+++ basxconnect/mailer_integration/models.py
@@ -6,7 +6,7 @@ from basxconnect.core.models import Person
 
 class Interest(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    external_id = models.CharField(max_length=50, unique=True)
+    external_id = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
