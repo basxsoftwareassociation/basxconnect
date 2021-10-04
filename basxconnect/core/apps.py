03cc19fb42faf957a9340490da4f28706c6141eb
Sam <sam@basx.dev>
2021-10-04 12:06:28 +0700

    add: automated creation of all vocabularies

--- basxconnect/core/apps.py
+++ basxconnect/core/apps.py
@@ -1,5 +1,27 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class CoreConfig(AppConfig):
     name = "basxconnect.core"
+
+    def ready(self):
+        from .models import Vocabulary
+
+        pre_installed_vocabulary = {
+            "tag": _("Tags"),
+            "title": _("Titles"),
+            "salutation": _("Salutations"),
+            "form_of_address": _("Forms Of Address"),
+            "gender": _("Genders"),
+            "naturaltype": _("Natural Person Types"),
+            "legaltype": _("Legal Person Types"),
+            "associationtype": _("Association Person Types"),
+            "addressstatus": _("Address statuses"),
+            "emailtype": _("Email Types"),
+            "urltype": _("URL Types"),
+            "phonetype": _("Phone Types"),
+            "addresstype": _("Address Types"),
+        }
+        for slug, name in pre_installed_vocabulary.items():
+            Vocabulary.objects.get_or_create(slug=slug, defaults={"name": name})
