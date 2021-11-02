a6b783025bd9b4a78ee7a05862832ea72b792f6f
Saksinkarn Petchkuljinda <=>
2021-11-02 13:26:42 +0700

    Update basxconnect/core/urls.py to match the new menu items.

--- basxconnect/core/urls.py
+++ basxconnect/core/urls.py
@@ -114,7 +114,6 @@ urlpatterns = [
     *default_model_paths(models.Postal),
     *default_model_paths(models.Phone),
     *default_model_paths(models.Web),
-    autopath(breadviews.system.systeminformation),
     autopath(settings_views.generalsettings),
     autopath(
         person_details_views.togglepersonstatus,
@@ -122,6 +121,7 @@ urlpatterns = [
     ),
     autopath(settings_views.personsettings),
     autopath(settings_views.relationshipssettings),
+    autopath(breadviews.system.systeminformation),
     autopath(settings_views.maintenancesettings),
     autopath(search_person_view.searchperson),
     autopath(
