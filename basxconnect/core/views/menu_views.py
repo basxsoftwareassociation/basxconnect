5aacc6eb702961e6191bd1ce8c91cd27754bbed2
Saksinkarn Petchkuljinda <=>
2021-11-03 14:14:08 +0700

    Added the developer mode check to check whether the Administration menu group should be visible.

--- basxconnect/core/views/menu_views.py
+++ basxconnect/core/views/menu_views.py
@@ -1,5 +1,6 @@
 from bread import layout as layout
 from bread import menu
+from bread.layout.base import DEVMODE_KEY
 from bread.utils.links import Link
 from bread.utils.urls import reverse, reverse_model
 from django.utils.translation import gettext_lazy as _
@@ -7,6 +8,15 @@ from django.utils.translation import gettext_lazy as _
 import basxconnect.core.models
 
 from .. import models
+
+
+# New menu group with developer mode checking
+class DevGroup(menu.Group):
+    def has_permission(self, request):
+        return super().has_permission(request) and request.session.get(
+            DEVMODE_KEY, False
+        )
+
 
 R = layout.grid.Row
 C = layout.grid.Col
@@ -16,7 +26,7 @@ F = layout.form.FormField
 
 persongroup = menu.Group(_("Persons"), iconname="group")
 settingsgroup = menu.Group(_("Settings"), iconname="settings", order=100)
-admingroup = menu.Group(_("Administration"), iconname="group")
+admingroup = DevGroup(_("Administration"), iconname="group")
 
 menu.registeritem(
     menu.Item(
@@ -52,6 +62,7 @@ menu.registeritem(
         settingsgroup,
     )
 )
+
 
 menu.registeritem(
     menu.Item(
