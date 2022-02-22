175eb707a6d9271a586952c6565c00f60306fed9
Pascal Wiesmann <pascal.wiesmann@outlook.com>
2022-02-22 13:30:00 +0200

    completely remove old mailchimp settings

--- basxconnect/mailer_integration/apps.py
+++ basxconnect/mailer_integration/apps.py
@@ -8,32 +8,3 @@ class MailerIntegrationConfig(AppConfig):
 
     def ready(self):
         import basxconnect.mailer_integration.signal_handlers  # noqa
-
-
-@register()
-def check_mailchimp_settings(app_configs, **kwargs):
-    required_settings = [
-        "MAILCHIMP_API_KEY",
-        "MAILCHIMP_SERVER",
-        "MAILCHIMP_LIST_ID",
-        "MAILCHIMP_SEGMENT_ID",
-        "MAILCHIMP_INTERESTS_CATEGORY_ID",
-    ]
-    errors = []
-    for setting in required_settings:
-        errors.extend(_check_setting(setting))
-
-    return errors
-
-
-def _check_setting(required_setting):
-    from django.conf import settings
-
-    if not hasattr(settings, required_setting):
-        error = Error(
-            f"setting.{required_setting} is not defined",
-            id=f"basxconnect.mailer_integration.ERROR{required_setting}",
-        )
-        return [error]
-    else:
-        return []
