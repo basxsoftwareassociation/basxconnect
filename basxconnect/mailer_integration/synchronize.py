2d4073fd1c07f75e98d7f96fa158a6b0c72ffee4
Pascal Wiesmann <pascal.wiesmann@outlook.com>
2022-02-28 15:26:56 +0200

    add new contacts only if status is subscribed

--- basxconnect/mailer_integration/synchronize.py
+++ basxconnect/mailer_integration/synchronize.py
@@ -82,7 +82,7 @@ def _get_or_create_tag(tag: str) -> models.Term:
 def is_valid_new_person(person: MailerPerson):
     return django_countries.Countries().countries.get(
         person.country
-    ) and person.status in ["subscribed", "unsubscribed", "cleaned"]
+    ) and person.status in ["subscribed"]
 
 
 def _save_sync_person(mailer_person, sync_result, syn_status):
